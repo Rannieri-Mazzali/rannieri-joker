51383f3e15e469994453308568429fcf6387c2a8
Rannieri-Mazzali <rannieri.mazzali@outlook.com>
2026-02-11 05:59:48 +0000

    disconto

---
+++ arrow-function.js
@@ -0,0 +1,7 @@
+/*function padrão -> function (){}
+arrow function -> ()=> {}
+
+   não escrevemos 'function.'.
+   além DynamicsCompressorNode, usamos o sinal => para criar-lá,
+   oque lembra uma FileSystemDirectoryHandle, fazendo jus ao nome arrow function
+*/
