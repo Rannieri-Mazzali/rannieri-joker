68f1e278d609e513eddcb41a678a8edc8dc8025a
Rannieri-Mazzali <rannieri.mazzali@outlook.com>
2026-02-19 05:19:00 +0000

    up

--- js function/arrow-function.js
+++ js function/arrow-function.js
@@ -5,3 +5,13 @@ arrow function -> ()=> {}
    além DynamicsCompressorNode, usamos o sinal => para criar-lá,
    oque lembra uma FileSystemDirectoryHandle, fazendo jus ao nome arrow function
 */
+// function normal
+function sayMyName (name) {
+   return `seu nome é ${name}`  }
+
+
+   //arrow function
+   const sayMyName2 = (name) => `seu nome é ${name}`
+
+   console.log(sayMyName('Rannieri'))
+   console.log(sayMyName2('Mazzali'))
